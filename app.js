da92e75fd46481644a572094eb83bd0bb18207b8
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-02-27 22:47:05 +0500

    Search Feature Added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var watches = {
-  rolex: {
-    rainbow_daytona: {
+  ROLEX: {
+    Rainbow_Daytona: {
       name: "Rolex - Rainbow Daytona",
       price: "12,03,99,720 PKR",
       gender: "Men's",
@@ -12,7 +12,7 @@ var watches = {
       src: "/imgs/rolex_daytona_rainbow.jpg",
     },
 
-    platinum_daytona: {
+    Platinum_Daytona: {
       name: "Rolex - Platinum Ice Blue Daytona",
       price: "4,61,99,729 PKR",
       gender: "Men's",
@@ -24,7 +24,7 @@ var watches = {
       src: "/imgs/platinum_daytona.jpg",
     },
 
-    gmt_master: {
+    GMT_Master_II: {
       name: "Rolex - GMT-Master II",
       price: "6,07,03,720 PKR",
       gender: "Men's",
@@ -36,7 +36,7 @@ var watches = {
       src: "/imgs/gmt-master_ii_yellow_gold.jpg",
     },
 
-    cosmograph_daytona: {
+    Cosmograph_Daytona: {
       name: "Rolex - Cosmograph Daytona",
       price: "71,20,541 PKR",
       gender: "Men's",
@@ -48,8 +48,8 @@ var watches = {
       src: "/imgs/rolex_cosmograph_daytona.jpg",
     },
 
-    daytona_stainless_steel: {
-      name: "Rolex- Daytona Stainless Steel",
+    Daytona_Stainless_Steel: {
+      name: "Rolex - Stainless Steel Daytona",
       price: "67,19,720 PKR",
       gender: "Men's",
       bezel: "Stainless Steel",
@@ -60,21 +60,21 @@ var watches = {
       src: "/imgs/silver-daytona.jpg",
     },
 
-    submariner: {
-        name: "Rolex- Submaruner Date White Gold",
-        price: "1,27,39,720 PKR",
-        gender: "Men's",
-        bezel: "Ceramic",
-        dialColor: "Blue",
-        bandMaterial: "White Gold",
-        glass: "Curved Sapphire Chrystal",
-        waterResistance: "300 Meters (1000 Feet)",
-        src: "/imgs/rolex-submariner.jpg",
-    }
+    Submariner: {
+      name: "Rolex- Submariner Date",
+      price: "1,27,39,720 PKR",
+      gender: "Men's",
+      bezel: "Ceramic",
+      dialColor: "Blue",
+      bandMaterial: "White Gold",
+      glass: "Curved Sapphire Chrystal",
+      waterResistance: "300 Meters (1000 Feet)",
+      src: "/imgs/rolex-submariner.jpg",
+    },
   },
 
-  omega: {
-    speed_master: {
+  OMEGA: {
+    Speed_Master: {
       name: "Omega - Speedmaster",
       price: "27,99,720 PKR",
       gender: "Men's",
@@ -86,50 +86,55 @@ var watches = {
       src: "/imgs/omega_speedmaster.jpg",
     },
 
-    seaMaster_planet: {
-        name: "Omega - Seamaster Planet Ocean",
-        price: "11,19,720 PKR",
-        gender: "Men's",
-        bezel: "Ceramic",
-        dialColor: "Black",
-        bandMaterial: "Stainless Steel",
-        glass: "Curved Sapphire Chrystal",
-        waterResistance: "600 Meters (2000 Feet)",
-        src: "/imgs/omega_seamaster.jpg",
-      },
+    Seamaster_Planet_Ocean: {
+      name: "Omega - Seamaster Planet Ocean",
+      price: "11,19,720 PKR",
+      gender: "Men's",
+      bezel: "Ceramic",
+      dialColor: "Black",
+      bandMaterial: "Stainless Steel",
+      glass: "Curved Sapphire Chrystal",
+      waterResistance: "600 Meters (2000 Feet)",
+      src: "/imgs/omega_seamaster.jpg",
+    },
 
-      seaMaster_olympic: {
-        name: "Omega - Seamaster Olympic",
-        price: "92,95,720 PKR",
-        gender: "Men's",
-        bezel: "Platinum",
-        dialColor: "Black",
-        bandMaterial: "Leather",
-        glass: "Curved Sapphire Chrystal",
-        waterResistance: "60 Meters (180 Feet)",
-        src: "/imgs/omega_seamaster_olympic.jpg",
-      },
+    Seamaster_Olympic: {
+      name: "Omega - Seamaster Olympic",
+      price: "92,95,720 PKR",
+      gender: "Men's",
+      bezel: "Platinum",
+      dialColor: "Black",
+      bandMaterial: "Leather",
+      glass: "Curved Sapphire Chrystal",
+      waterResistance: "60 Meters (180 Feet)",
+      src: "/imgs/omega_seamaster_olympic.jpg",
+    },
 
-      seaMaster_diver: {
-        name: "Omega - Seamaster Olympic",
-        price: "8,95,720 PKR",
-        gender: "Men's",
-        bezel: "Stainless Steel Domed",
-        dialColor: "Black",
-        bandMaterial: "Stainless Steel",
-        glass: "Curved Sapphire Chrystal",
-        waterResistance: "30 Meters (90 Feet)",
-        src: "/imgs/omega_seamaster_diver.jpg",
-      },
+    Seamaster_Diver: {
+      name: "Omega - Seamaster Stainless Steel",
+      price: "8,95,720 PKR",
+      gender: "Men's",
+      bezel: "Stainless Steel Domed",
+      dialColor: "Black",
+      bandMaterial: "Stainless Steel",
+      glass: "Curved Sapphire Chrystal",
+      waterResistance: "30 Meters (90 Feet)",
+      src: "/imgs/omega_seamaster_diver.jpg",
+    },
   },
 };
 
 var main = document.getElementById("main");
+var brand = document.getElementById("brand");
+var collection = document.getElementById("collection");
+brand.innerHTML = `<option>Select Brand</option>`;
+collection.innerHTML = `<option>Select Collection</option>`;
 
 for (var key in watches) {
+  console.log(key);
+  brand.innerHTML += `<option value="${key.toLocaleUpperCase()}">${key.toLocaleUpperCase()}</option>`;
   for (var key1 in watches[key]) {
     var obj = watches[key][key1];
-    console.log(obj);
     main.innerHTML += `
                    <div
               class="col-12 col-sm-6 col-lg-4 card"
@@ -175,10 +180,71 @@ for (var key in watches) {
   }
 }
 
-// CROWN Screw Down
-// BEZELYellow Gold
-// MOVEMENTAutomatic
-// CASE BACKSolid
-// MATERIALYellow Gold
-// CASE SIZE40 mm
-// CASE SHAPERound
+collection.innerText = "";
+
+function setCollection() {
+  collection.innerHTML = `<option>Select Collection</option>`;
+  for (var key in watches[brand.value]) {
+    collection.innerHTML += `<option value="${key}">${key.replaceAll(
+      "_",
+      " "
+    )}</option>`;
+  }
+}
+
+var watchDetails = document.getElementById("watch-details");
+watchDetails.style.display = "none";
+
+function searchWatch() {
+    if (brand.value !== "Select Brand" || collection.value !== "Select Collection") {
+  main.style.display = "none";
+  watchDetails.style.display = "flex";
+
+  var searchedWatch = watches[brand.value][collection.value];
+  watchDetails.innerHTML = `
+  <div class="card mb-3 p-3 border-0">
+  <div class="row g-0 align-items-center justify-content-center">
+    <div class="col-md-3 d-flex justify-content-center align-itmes-center">
+      <img
+        src="${searchedWatch.src}"
+        class="search-img img-fluid rounded-start"
+        alt="..."
+      />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <h5 class="card-title fs-4">
+      ${searchedWatch.name}
+    </h5>
+    <p
+      class="price p-2 fs-5 fw-semibold mb-3 mt-3 border border-1 rounded-2 w-75"
+      style="color: red"
+    >
+      ${searchedWatch.price}
+    </p>
+    <p class="card-text">
+      Gender:
+      <span class="fw-semibold">${searchedWatch.gender}</span>
+    </p>
+    <p class="card-text">
+      Dial Color: <span class="fw-semibold">${searchedWatch.dialColor}</span>
+    </p>
+    <p class="card-text">
+      Bazel: <span class="fw-semibold">${searchedWatch.bezel}</span>
+    </p>
+    <p class="card-text">
+      Band Material: <span class="fw-semibold">${searchedWatch.bandMaterial}</span>
+    </p>
+    <p class="card-text">
+      Glass: <span class="fw-semibold">${searchedWatch.glass}</span>
+    </p>
+    <p class="card-text">
+      Water Resistance: <span class="fw-semibold">${searchedWatch.waterResistance}</span>
+    </p>
+       </div>
+    </div>
+    </div>
+   </div>
+  `;
+    }
+}
