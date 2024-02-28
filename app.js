d4b0ccdb7b164c51c031887f39856d237a9de20a
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-02-29 00:31:32 +0500

    Made changes and added features
    >Change Color of navbar
    >Reset of selections on 'clear' button
    >Added Dark mode
    >Fixed Menu option for small screens
    >Added Footer

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+function myFunction() {
+    var element = document.body;
+    element.dataset.bsTheme =
+      element.dataset.bsTheme == "light" ? "dark" : "light";
+  }
+
 var watches = {
   ROLEX: {
     Rainbow_Daytona: {
@@ -123,31 +129,31 @@ var watches = {
     },
   },
 
-  PATEK_PHILIPPE : {
+  PATEK_PHILIPPE: {
     Grand_Complications_Rose_Gold: {
-        name: "Patek Philippe - Split Seconds",
-        price: "5,73,99,720 PKR",
-        gender: "Men's",
-        bezel: "Rose Gold",
-        dialColor: "Black",
-        bandMaterial: "Rose Gold",
-        glass: "Curved Sapphire Chrystal",
-        waterResistance: "30 Meters (100 Feet)",
-        src: "/imgs/patek_philippe_gold.png",
+      name: "Patek Philippe - Split Seconds",
+      price: "5,73,99,720 PKR",
+      gender: "Men's",
+      bezel: "Rose Gold",
+      dialColor: "Black",
+      bandMaterial: "Rose Gold",
+      glass: "Curved Sapphire Chrystal",
+      waterResistance: "30 Meters (100 Feet)",
+      src: "/imgs/patek_philippe_gold.png",
     },
 
     Grand_Complications_Diamond: {
-        name: "Patek Philippe - Grandmaster Chime",
-        price: "1,25,99,99,720 PKR",
-        gender: "Men's",
-        bezel: "Set with Diamonds",
-        dialColor: "Black",
-        bandMaterial: "White Gold",
-        glass: "Curved Sapphire Chrystal",
-        waterResistance: "None",
-        src: "/imgs/patek_philippe_diamond.jpg",
-    }
-  }
+      name: "Patek Philippe - Grandmaster Chime",
+      price: "1,25,99,99,720 PKR",
+      gender: "Men's",
+      bezel: "Set with Diamonds",
+      dialColor: "Black",
+      bandMaterial: "White Gold",
+      glass: "Curved Sapphire Chrystal",
+      waterResistance: "None",
+      src: "/imgs/patek_philippe_diamond.jpg",
+    },
+  },
 };
 
 var main = document.getElementById("main");
@@ -157,9 +163,10 @@ brand.innerHTML = `<option>Select Brand</option>`;
 collection.innerHTML = `<option>Select Collection</option>`;
 
 for (var key in watches) {
-    console.log(key);
-    brand.innerHTML += `<option value="${key.toLocaleUpperCase()}">${key.toLocaleUpperCase().replace("_", " ")}</option>`;
-} 
+  brand.innerHTML += `<option value="${key.toUpperCase()}">${key
+    .toUpperCase()
+    .replace("_", " ")}</option>`;
+}
 
 function renderAllWatches() {
   for (var key in watches) {
@@ -215,6 +222,7 @@ renderAllWatches();
 
 collection.innerText = "";
 
+collection.innerHTML = `<option>Select Collection</option>`;
 function setCollection() {
   collection.innerHTML = `<option>Select Collection</option>`;
   for (var key in watches[brand.value]) {
@@ -230,7 +238,7 @@ watchDetails.style.display = "none";
 
 function searchWatch() {
   if (
-    brand.value !== "Select Brand" ||
+    brand.value !== "Select Brand" &&
     collection.value !== "Select Collection"
   ) {
     main.style.display = "none";
@@ -282,12 +290,75 @@ function searchWatch() {
     </div>
    </div>
   `;
+  } else if (
+    brand.value !== "Select Brand" &&
+    collection.value === "Select Collection"
+  ) {
+    main.innerHTML = "";
+    main.style.display = "flex";
+
+    watchDetails.style.display = "none";
+
+    for (var key1 in watches[brand.value]) {
+      var obj = watches[brand.value][key1];
+      main.innerHTML += `
+        <div
+   class="col-12 col-sm-6 col-lg-4 card"
+   style="width: 18rem"
+ >
+   <img
+     src="${obj.src}"
+     class="card-img-top pt-3 pb-3 border bottom-border-1"
+     alt=""
+   />
+   <div class="card-body">
+     <h5 class="card-title fs-4">
+       ${obj.name}
+     </h5>
+     <p
+       class="price p-2 fs-5 fw-semibold mb-3 mt-3 border border-1 rounded-2 w-75"
+       style="color: red"
+     >
+       ${obj.price}
+     </p>
+     <p class="card-text">
+       Gender:
+       <span class="fw-semibold">${obj.gender}</span>
+     </p>
+     <p class="card-text">
+       Dial Color: <span class="fw-semibold">${obj.dialColor}</span>
+     </p>
+     <p class="card-text">
+       Bazel: <span class="fw-semibold">${obj.bezel}</span>
+     </p>
+     <p class="card-text">
+       Band Material: <span class="fw-semibold">${obj.bandMaterial}</span>
+     </p>
+     <p class="card-text">
+       Glass: <span class="fw-semibold">${obj.glass}</span>
+     </p>
+     <p class="card-text">
+       Water Resistance: <span class="fw-semibold">${obj.waterResistance}</span>
+     </p>
+   </div>
+ </div>
+`;
+    }
   }
 }
 
 function clearSearch() {
-    main.innerHTML = "";
-    main.style.display = "flex";
-    watchDetails.style.display = "none";
-    renderAllWatches();
+  main.innerHTML = "";
+  main.style.display = "flex";
+  watchDetails.style.display = "none";
+  brand.innerHTML = `<option>Select Brand</option>`;
+  for (var key in watches) {
+    brand.innerHTML += `<option value="${key.toUpperCase()}">${key
+      .toUpperCase()
+      .replace("_", " ")}</option>`;
   }
+  collection.innerHTML = `<option>Select Collection</option>`;
+
+  renderAllWatches();
+
+}
