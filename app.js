eb95f4551c5acf39faaee94553b8d7828328e5e0
Abdul-Bari-Mazari <mazari3339@gmail.com>
2024-02-28 16:07:51 +0500

    Added Patek Philippe watches

--- app.js
+++ app.js
@@ -122,6 +122,32 @@ var watches = {
       src: "/imgs/omega_seamaster_diver.jpg",
     },
   },
+
+  PATEK_PHILIPPE : {
+    Grand_Complications_Rose_Gold: {
+        name: "Patek Philippe - Split Seconds",
+        price: "5,73,99,720 PKR",
+        gender: "Men's",
+        bezel: "Rose Gold",
+        dialColor: "Black",
+        bandMaterial: "Rose Gold",
+        glass: "Curved Sapphire Chrystal",
+        waterResistance: "30 Meters (100 Feet)",
+        src: "/imgs/patek_philippe_gold.png",
+    },
+
+    Grand_Complications_Diamond: {
+        name: "Patek Philippe - Grandmaster Chime",
+        price: "1,25,99,99,720 PKR",
+        gender: "Men's",
+        bezel: "Set with Diamonds",
+        dialColor: "Black",
+        bandMaterial: "White Gold",
+        glass: "Curved Sapphire Chrystal",
+        waterResistance: "None",
+        src: "/imgs/patek_philippe_diamond.jpg",
+    }
+  }
 };
 
 var main = document.getElementById("main");
@@ -131,54 +157,61 @@ brand.innerHTML = `<option>Select Brand</option>`;
 collection.innerHTML = `<option>Select Collection</option>`;
 
 for (var key in watches) {
-  console.log(key);
-  brand.innerHTML += `<option value="${key.toLocaleUpperCase()}">${key.toLocaleUpperCase()}</option>`;
-  for (var key1 in watches[key]) {
-    var obj = watches[key][key1];
-    main.innerHTML += `
-                   <div
-              class="col-12 col-sm-6 col-lg-4 card"
-              style="width: 18rem"
-            >
-              <img
-                src="${obj.src}"
-                class="card-img-top pt-3 pb-3 border bottom-border-1"
-                alt=""
-              />
-              <div class="card-body">
-                <h5 class="card-title fs-4">
-                  ${obj.name}
-                </h5>
-                <p
-                  class="price p-2 fs-5 fw-semibold mb-3 mt-3 border border-1 rounded-2 w-75"
-                  style="color: red"
+    console.log(key);
+    brand.innerHTML += `<option value="${key.toLocaleUpperCase()}">${key.toLocaleUpperCase().replace("_", " ")}</option>`;
+} 
+
+function renderAllWatches() {
+  for (var key in watches) {
+    for (var key1 in watches[key]) {
+      var obj = watches[key][key1];
+      main.innerHTML += `
+                       <div
+                  class="col-12 col-sm-6 col-lg-4 card"
+                  style="width: 18rem"
                 >
-                  ${obj.price}
-                </p>
-                <p class="card-text">
-                  Gender:
-                  <span class="fw-semibold">${obj.gender}</span>
-                </p>
-                <p class="card-text">
-                  Dial Color: <span class="fw-semibold">${obj.dialColor}</span>
-                </p>
-                <p class="card-text">
-                  Bazel: <span class="fw-semibold">${obj.bezel}</span>
-                </p>
-                <p class="card-text">
-                  Band Material: <span class="fw-semibold">${obj.bandMaterial}</span>
-                </p>
-                <p class="card-text">
-                  Glass: <span class="fw-semibold">${obj.glass}</span>
-                </p>
-                <p class="card-text">
-                  Water Resistance: <span class="fw-semibold">${obj.waterResistance}</span>
-                </p>
-              </div>
-            </div>
-       `;
+                  <img
+                    src="${obj.src}"
+                    class="card-img-top pt-3 pb-3 border bottom-border-1"
+                    alt=""
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title fs-4">
+                      ${obj.name}
+                    </h5>
+                    <p
+                      class="price p-2 fs-5 fw-semibold mb-3 mt-3 border border-1 rounded-2 w-75"
+                      style="color: red"
+                    >
+                      ${obj.price}
+                    </p>
+                    <p class="card-text">
+                      Gender:
+                      <span class="fw-semibold">${obj.gender}</span>
+                    </p>
+                    <p class="card-text">
+                      Dial Color: <span class="fw-semibold">${obj.dialColor}</span>
+                    </p>
+                    <p class="card-text">
+                      Bazel: <span class="fw-semibold">${obj.bezel}</span>
+                    </p>
+                    <p class="card-text">
+                      Band Material: <span class="fw-semibold">${obj.bandMaterial}</span>
+                    </p>
+                    <p class="card-text">
+                      Glass: <span class="fw-semibold">${obj.glass}</span>
+                    </p>
+                    <p class="card-text">
+                      Water Resistance: <span class="fw-semibold">${obj.waterResistance}</span>
+                    </p>
+                  </div>
+                </div>
+           `;
+    }
   }
 }
+
+renderAllWatches();
 
 collection.innerText = "";
 
@@ -196,12 +229,15 @@ var watchDetails = document.getElementById("watch-details");
 watchDetails.style.display = "none";
 
 function searchWatch() {
-    if (brand.value !== "Select Brand" || collection.value !== "Select Collection") {
-  main.style.display = "none";
-  watchDetails.style.display = "flex";
+  if (
+    brand.value !== "Select Brand" ||
+    collection.value !== "Select Collection"
+  ) {
+    main.style.display = "none";
+    watchDetails.style.display = "flex";
 
-  var searchedWatch = watches[brand.value][collection.value];
-  watchDetails.innerHTML = `
+    var searchedWatch = watches[brand.value][collection.value];
+    watchDetails.innerHTML = `
   <div class="card mb-3 p-3 border-0">
   <div class="row g-0 align-items-center justify-content-center">
     <div class="col-md-3 d-flex justify-content-center align-itmes-center">
@@ -246,5 +282,12 @@ function searchWatch() {
     </div>
    </div>
   `;
-    }
+  }
 }
+
+function clearSearch() {
+    main.innerHTML = "";
+    main.style.display = "flex";
+    watchDetails.style.display = "none";
+    renderAllWatches();
+  }
